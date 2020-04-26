c6c389a9aa44057a3af39ff4dcc3fc1dbfd03d0c
Hallison Oliveira <hallisoncwb@gmail.com>
2020-04-26 18:20:26 -0300

    Create project

---
+++ src/main/kotlin/br/com/hallisonoliveira/appdev/exercicio2/Exercicio2Application.kt
@@ -0,0 +1,11 @@
+package br.com.hallisonoliveira.appdev.exercicio2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Exercicio2Application
+
+fun main(args: Array<String>) {
+	runApplication<Exercicio2Application>(*args)
+}
